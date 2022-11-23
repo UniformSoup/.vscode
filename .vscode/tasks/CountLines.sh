59d0ff2430a7aa005faca08485e125af333c4244
UniformSoup <halad@outlook.com>
2022-11-23 00:30:52 +0000

    Fixed Path Varaible without args (gdb happy)

--- .vscode/tasks/CountLines.sh
+++ .vscode/tasks/CountLines.sh
@@ -2,6 +2,6 @@
 
 echo -n "Lines of C and C++: "
 
-/usr/bin/find -regex '.*\.\(cpp\|hpp\|\c\|h\|cc\|hh\|cxx\|hxx\)$' | \
+find -regex '.*\.\(cpp\|hpp\|\c\|h\|cc\|hh\|cxx\|hxx\)$' | \
     grep -E -i -v '^./(Debug|Release)' | \
         xargs awk 'END{print NR}'
