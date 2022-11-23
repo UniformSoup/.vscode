eb3645a90c0411772f1fb0f5a1ba3ffb89fe56d2
UniformSoup <halad@outlook.com>
2022-11-23 00:47:33 +0000

    View Assembly bug when the directory not workspace

--- .vscode/tasks/ViewAssembly.sh
+++ .vscode/tasks/ViewAssembly.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-if [[ $1 =~ .(cpp|hpp|c|h|cc|hh|cxx|hxx)$ ]];
+filename=$(basename -- "$1")
+
+if [[ $1 =~ \.(cpp|hpp|c|h|cc|hh|cxx|hxx)$ ]];
 then
     gcc -fno-asynchronous-unwind-tables -fno-exceptions \
         -funroll-loops -fno-rtti -O -masm=intel -march=haswell -fverbose-asm \
-        -S "$1" -o "/tmp/$1.s"
-    code "/tmp/$1.s"
+        -S "$1" -o "/tmp/$filename.s"
+    code "/tmp/$filename.s"
 else
     echo "The current file is not a C or C++ file extension."
 fi
