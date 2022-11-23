b3617700ad53901f61ce8244db3bef8f09cc4e30
UniformSoup <halad@outlook.com>
2022-11-23 16:18:24 +0000

    Tasks print nicer + fixed assembly

--- .vscode/tasks/ViewAssembly.sh
+++ .vscode/tasks/ViewAssembly.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-filename=$(basename -- "$1")
-
 if [[ $1 =~ \.(cpp|hpp|c|h|cc|hh|cxx|hxx)$ ]];
 then
-    temp=$(mktemp XXX --suffix .$filename.s)
-    gcc -fno-asynchronous-unwind-tables -fno-exceptions \
-        -funroll-loops -fno-rtti -O -masm=intel -march=haswell -fverbose-asm \
-        -S "$1" -o $temp
+    temp=$(mktemp /tmp/XXX --suffix .$(basename $1).s)
+    gcc -fno-asynchronous-unwind-tables -fno-exceptions -ffunction-sections -fdata-sections \
+        -funroll-loops -fno-rtti -Os -masm=intel -march=haswell -fverbose-asm \
+        -S "$1" -o "$temp"
     code $temp
+    echo "Viewing Assembly For: $1"
 else
-    echo "The current file is not a C or C++ file extension."
+    echo "$1 is not a C or C++ file."
 fi
