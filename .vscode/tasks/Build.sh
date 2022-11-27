d0e156c23a4c4c1b13ea340977bda6249f6ffc20
UniformSoup <halad@outlook.com>
2022-11-27 02:16:56 +0000

    Build warns if cmake dirs do not exist.

--- .vscode/tasks/Build.sh
+++ .vscode/tasks/Build.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-while read -r line 
-do
-    line=${line//$'\r'/}
-    if [ "$line" == "ninja: no work to do." ];
-    then
-        echo "Nothing to do."
-    else
-        echo "$line"
-    fi
-done < <(cmake --build $1)
+if [ -d $1 ];
+then
+    while read -r line 
+    do
+        line=${line//$'\r'/}
+        if [ "$line" == "ninja: no work to do." ];
+        then
+            echo "Nothing to do."
+        else
+            echo "$line"
+        fi
+    done < <(cmake --build $1)
+else
+    echo "$1 directory does not exist, you may need to configure CMake."
+fi
