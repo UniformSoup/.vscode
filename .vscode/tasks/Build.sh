3b038b1881b29d6de5bc5b5bf1f03aa03945bd57
UniformSoup <halad@outlook.com>
2022-11-28 13:50:53 +0000

    Fixed build stinky (executed even if CMake fails)

--- .vscode/tasks/Build.sh
+++ .vscode/tasks/Build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+# Return Code Trick: https://stackoverflow.com/a/43901140/13357306
 if [ -d $1 ];
 then
-    while read -r line 
+    while read -r line || { ec=$line && break; }
     do
         line=${line//$'\r'/}
         if [ "$line" == "ninja: no work to do." ];
@@ -11,7 +12,9 @@ then
         else
             echo "$line"
         fi
-    done < <(cmake --build $1)
+    done < <(cmake --build $1; printf $?)
+    exit $ec
 else
     echo "$1 directory does not exist, you may need to configure CMake."
+    exit 1
 fi
