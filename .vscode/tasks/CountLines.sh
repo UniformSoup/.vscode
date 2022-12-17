a217034c3176c9801406cb16da24414fa093b8b0
UniformSoup <halad@outlook.com>
2022-12-17 15:34:05 +0000

    Add Excluded Directories

--- .vscode/tasks/CountLines.sh
+++ .vscode/tasks/CountLines.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo -n "Lines of C and C++: "
+excluded_directories=('Debug' 'Release' '.vscode')
 
+excluded_directories=$(printf "|%s" "${excluded_directories[@]}")
+excluded_directories=${excluded_directories:1}
+
+echo -n "Lines of C and C++: "
 find -regex '.*\.\(cpp\|hpp\|\c\|h\|cc\|hh\|cxx\|hxx\)$' | \
-    grep -E -i -v '^./(Debug|Release)' | \
+    grep -E -i -v "^./($excluded_directories)" | \
         xargs awk 'END{print NR}'
