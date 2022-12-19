18e54ed85efe6242a68dbd2589967d9ba285772a
UniformSoup <halad@outlook.com>
2022-12-19 00:06:12 +0000

    Moved some statements around.

--- .vscode/tasks/CountLines.sh
+++ .vscode/tasks/CountLines.sh
@@ -2,10 +2,11 @@
 
 excluded_directories=('Debug' 'Release' '.vscode')
 
+echo -n "Lines of C and C++: "
+
 excluded_directories=$(printf "|%s" "${excluded_directories[@]}")
 excluded_directories=${excluded_directories:1}
 
-echo -n "Lines of C and C++: "
 find -regex '.*\.\(cpp\|hpp\|\c\|h\|cc\|hh\|cxx\|hxx\)$' | \
     grep -E -i -v "^./($excluded_directories)" | \
         xargs awk 'END{print NR}'
