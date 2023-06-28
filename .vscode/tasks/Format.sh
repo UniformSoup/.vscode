df5f3d811182f64b1f2087c7bfe9c37b8ebdb2c2
UniformSoup <halad@outlook.com>
2023-06-28 18:33:33 +0100

    make worky

--- .vscode/tasks/Format.sh
+++ .vscode/tasks/Format.sh
@@ -4,7 +4,7 @@ excluded_directories=('Debug' 'Release' '.vscode' 'Doxygen')
 excluded_directories=$(printf "|%s" "${excluded_directories[@]}")
 excluded_directories=${excluded_directories:1}
 
-format_files=$(find -regex '.*\.\(cpp\|hpp\|\c\|h\|cc\|hh\|cxx\|hxx\)$' | \
+format_files=$(find . -regex '.*\.\(cpp\|hpp\|\c\|h\|cc\|hh\|cxx\|hxx\)$' | \
     grep -E -i -v "^./($excluded_directories)")
 
 for file in $format_files
