faebe6507446eb21fb37399793325883462284a7
UniformSoup <halad@outlook.com>
2023-06-30 17:58:47 +0100

    Updated to use grep twice

--- .vscode/tasks/CountLines.sh
+++ .vscode/tasks/CountLines.sh
@@ -7,6 +7,6 @@ echo -n "Lines of C and C++: "
 excluded_directories=$(printf "|%s" "${excluded_directories[@]}")
 excluded_directories=${excluded_directories:1}
 
-find -regex '.*\.\(cpp\|hpp\|\c\|h\|cc\|hh\|cxx\|hxx\)$' | \
-    grep -E -i -v "^./($excluded_directories)" | \
-        xargs awk 'END{print NR}'
+find . | grep ".*\.\(cpp\|hpp\|\c\|h\|cc\|hh\|cxx\|hxx\)$" | grep -Eiv "^./($excluded_directories)" | xargs awk 'END{print NR}'
+
+        
