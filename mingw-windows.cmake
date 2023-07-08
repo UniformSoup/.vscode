74f4ee1882ee2f5efd8aeebdce78e0e067c54b94
UniformSoup <halad@outlook.com>
2023-07-08 20:59:49 +0100

    use clang

--- mingw-windows.cmake
+++ mingw-windows.cmake
@@ -1,7 +1,7 @@
 set(CMAKE_SYSTEM_NAME Windows)
 
-set(CMAKE_C_COMPILER gcc)
-set(CMAKE_CXX_COMPILER g++)
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang++)
 
 set(CMAKE_C_FLAGS "-Wall -Wfatal-errors -fdiagnostics-color=always")
 set(CMAKE_CXX_FLAGS ${CMAKE_C_FLAGS})
