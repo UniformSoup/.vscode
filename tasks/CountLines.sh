90d3035aefd628f2115ffd701eb321dd4557c58a
UniformSoup <halad@outlook.com>
2023-07-09 21:31:54 +0100

    add common excluded dirs

--- tasks/CountLines.sh
+++ tasks/CountLines.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-excluded_directories=('Debug' 'Release' '.vscode')
+excluded_directories=('Debug' 'Release' '.vscode' 'glm' 'glfw')
 
 echo -n "Lines of C and C++: "
 
@@ -8,5 +8,3 @@ excluded_directories=$(printf "|%s" "${excluded_directories[@]}")
 excluded_directories=${excluded_directories:1}
 
 find . | grep ".*\.\(cpp\|hpp\|\c\|h\|cc\|hh\|cxx\|hxx\)$" | grep -Eiv "^./($excluded_directories)" | xargs awk 'END{print NR}'
-
-        
